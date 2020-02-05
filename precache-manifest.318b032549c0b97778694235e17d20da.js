75b10ac3384dcc1cd1d4155bed01e75ef75cc2c0
weianofsteel <skywalker0631@gmail.com>
2020-02-06 01:49:44 +0800

    Updates

---
+++ precache-manifest.318b032549c0b97778694235e17d20da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a744e9ffe4932b78d24493267514c6ef",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "38272f2c70fcad416c49",
+    "url": "/react-weather-app/static/js/2.137a3c33.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-weather-app/static/js/2.137a3c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd09aaee03e2b3afb5c5",
+    "url": "/react-weather-app/static/js/main.f235c12b.chunk.js"
+  },
+  {
+    "revision": "d2857d5f4a409e252419",
+    "url": "/react-weather-app/static/js/runtime-main.a75341be.js"
+  }
+]);
